6ea7283d653eed601ae6c25f90e527e02bbd50b7
Jerel Miller <jerelmiller@gmail.com>
2017-04-23 02:21:18 -0700

    Add restore to test authenticators

--- test/utils/authenticators.js
+++ test/utils/authenticators.js
@@ -2,15 +2,18 @@ import { createAuthenticator } from '../../src'
 
 export const spiedAuthenticator = createAuthenticator({
   name: 'test',
-  authenticate: jest.fn(data => Promise.resolve(data))
+  authenticate: jest.fn(data => Promise.resolve(data)),
+  restore: jest.fn(() => Promise.resolve())
 })
 
 export const successAuthenticator = createAuthenticator({
   name: 'test',
-  authenticate: data => Promise.resolve({ token: 'abcdefg' })
+  authenticate: data => Promise.resolve({ token: 'abcdefg' }),
+  restore: () => Promise.resolve()
 })
 
 export const failAuthenticator = createAuthenticator({
   name: 'test',
-  authenticate: () => Promise.reject()
+  authenticate: () => Promise.reject(),
+  restore: () => Promise.reject()
 })
