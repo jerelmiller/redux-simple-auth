c0c272a64678f661cfcaa9c08b312c9dee079b13
Jerel Miller <jerelmiller@gmail.com>
2017-04-29 02:31:36 -0700

    Get rid of createAuthorizer

--- src/createAuthorizer.js
+++
@@ -1,6 +0,0 @@
-const createAuthorizer = ({ name, authorize }) => ({
-  name,
-  authorize
-})
-
-export default createAuthorizer
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 export { default as createAuthMiddleware } from './middleware'
 export { default as reducer } from './reducer'
 export { default as createAuthenticator } from './createAuthenticator'
-export { default as createAuthorizer } from './createAuthorizer'
 export {
   authenticate,
   fetch,
