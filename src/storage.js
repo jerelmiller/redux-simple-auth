78d8707e393d8948b65f340c5dad478973f60115
Jerel Miller <jerelmiller@gmail.com>
2017-08-05 21:39:49 -0700

    Move storage.js to folder as index.js

--- src/storage.js
+++
@@ -1,3 +0,0 @@
-export { default as createAdaptiveStore } from './storage/adaptive'
-export { default as createLocalStorageStore } from './storage/localStorage'
-export { default as createCookieStore } from './storage/cookie'
---
+++ src/storage/index.js
@@ -0,0 +1,3 @@
+export { default as createAdaptiveStore } from './adaptive'
+export { default as createLocalStorageStore } from './localStorage'
+export { default as createCookieStore } from './cookie'
