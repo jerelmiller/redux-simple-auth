dc6307fb9a925030b8fad2eba6ddd66ebf5ae191
Jerel Miller <jerelmiller@gmail.com>
2017-08-05 21:42:15 -0700

    Move storage export to main export

--- src/storage/index.js
+++
@@ -1,3 +0,0 @@
-export { default as createAdaptiveStore } from './adaptive'
-export { default as createLocalStorageStore } from './localStorage'
-export { default as createCookieStore } from './cookie'
