c587c176b432aa537bf70529d26eac0e7ea89872
Jerel Miller <jerelmiller@gmail.com>
2017-08-05 22:20:59 -0700

    Add an enhancer to define initial state

---
+++ src/enhancer.js
@@ -0,0 +1,29 @@
+import isPlainObject from 'lodash.isplainobject'
+import { initialize } from './actions'
+import reducer from './reducer'
+
+const validateStorage = storage => {
+  if (!isPlainObject(storage) || storage.restore == null) {
+    throw new Error(
+      'Expected `storage` to be a valid storage. You either forgot to ' +
+        'include it or you passed an invalid storage object'
+    )
+  }
+}
+
+const enhancer = ({ storage } = {}) => createStore => (
+  rootReducer,
+  preloadedState,
+  enhancer
+) => {
+  validateStorage(storage)
+
+  const initialState = {
+    session: reducer(null, initialize(storage.restore())),
+    ...preloadedState
+  }
+
+  return createStore(rootReducer, initialState, enhancer)
+}
+
+export default enhancer
