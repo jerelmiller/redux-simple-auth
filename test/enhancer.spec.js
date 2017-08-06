0f0f533da69021fb24b91beffebbbaadab2454c4
Jerel Miller <jerelmiller@gmail.com>
2017-08-05 22:52:39 -0700

    Write test to verify enhancer

--- test/enhancer.spec.js
+++ test/enhancer.spec.js
@@ -7,4 +7,34 @@ describe('enhancer', () => {
 
     expect(enhancedCreateStore).toBeInstanceOf(Function)
   })
+
+  it('sets up store with initial storage state', () => {
+    const mock = jest.fn()
+    const dummyData = {
+      authenticated: {
+        authenticator: 'dummy',
+        token: 'abcde'
+      }
+    }
+    const storage = createMockStorage(dummyData)
+    const createStore = jest.fn()
+
+    const enhancedCreateStore = enhancer({ storage })(createStore)(
+      mock,
+      null,
+      mock
+    )
+
+    expect(createStore).toHaveBeenCalledWith(
+      mock,
+      {
+        session: {
+          authenticator: 'dummy',
+          data: { token: 'abcde' },
+          isAuthenticated: false
+        }
+      },
+      mock
+    )
+  })
 })
