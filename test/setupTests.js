6b18f778f0a3686050bcc414fc4ceafd0a3340ff
Jerel Miller <jerelmiller@gmail.com>
2017-10-08 21:12:42 -0700

    Add a session storage mock

--- test/setupTests.js
+++ test/setupTests.js
@@ -19,5 +19,25 @@ const createLocalStorageMock = () => {
   }
 }
 
+const createSessionStorageMock = () => {
+  let store = {}
+
+  return {
+    setItem(key, value) {
+      store[key] = value.toString()
+    },
+    getItem(key) {
+      return store[key] || null
+    },
+    removeItem(key) {
+      delete store[key]
+    },
+    clear() {
+      store = {}
+    }
+  }
+}
+
 global.fetch = fetch
 global.localStorage = createLocalStorageMock()
+global.sessionStorage = createSessionStorageMock()
