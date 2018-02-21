e187c1020864db14148bc3b4df9b2d0c6be49088
Jerel Miller <jerelmiller@gmail.com>
2018-02-21 00:24:32 -0800

    Add ProtectedRoute component

--- packages/react-redux-simple-auth/src/index.js
+++ packages/react-redux-simple-auth/src/index.js
@@ -1,2 +1,6 @@
+export { default as ProtectedRoute } from './components/ProtectedRoute'
+export {
+  default as RouteUnlessAuthenticated
+} from './components/RouteUnlessAuthenticated'
 export { default as Session } from './components/Session'
 export { default as WhenRestored } from './components/WhenRestored'
