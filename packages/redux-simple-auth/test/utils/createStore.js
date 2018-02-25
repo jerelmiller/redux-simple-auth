c6b6a6c97b588ea1aff9e47518fa4a7af6dbaae2
Jerel Miller <jerelmiller@gmail.com>
2018-02-25 02:39:03 -0800

    Use combine reducers in test store

--- packages/redux-simple-auth/test/utils/createStore.js
+++ packages/redux-simple-auth/test/utils/createStore.js
@@ -1,5 +1,9 @@
-import { applyMiddleware, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 import reducer from '../../src/reducer'
 
 export default ({ middleware, initialState = {} }) =>
-  createStore(reducer, initialState, applyMiddleware(middleware))
+  createStore(
+    combineReducers({ session: reducer }),
+    initialState,
+    applyMiddleware(middleware)
+  )
