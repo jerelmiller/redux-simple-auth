ce792e05c1fb9758247f484d102d79aeb682ce6d
Jerel Miller <jerelmiller@gmail.com>
2018-02-24 23:14:42 -0800

    Get rid of useless tests

--- packages/redux-simple-auth/test/middleware.spec.js
+++ packages/redux-simple-auth/test/middleware.spec.js
@@ -34,42 +34,6 @@ afterEach(() => {
   storage.restore.mockClear()
 })
 
-it('returns a function that handles {getState, dispatch}', () => {
-  const middleware = createAuthMiddleware({
-    storage,
-    authenticator: spiedAuthenticator
-  })
-  expect(middleware).toBeInstanceOf(Function)
-  expect(middleware.length).toBe(1)
-})
-
-describe('store handler', () => {
-  it('returns function that handles next', () => {
-    const middleware = createAuthMiddleware({
-      storage,
-      authenticator: spiedAuthenticator
-    })
-    const nextHandler = middleware({})
-
-    expect(nextHandler).toBeInstanceOf(Function)
-    expect(nextHandler.length).toBe(1)
-  })
-})
-
-describe('action handler', () => {
-  it('action handler returns a function that handles action', () => {
-    const middleware = createAuthMiddleware({
-      storage,
-      authenticator: spiedAuthenticator
-    })
-    const nextHandler = middleware({})
-    const actionHandler = nextHandler()
-
-    expect(actionHandler).toBeInstanceOf(Function)
-    expect(actionHandler.length).toBe(1)
-  })
-})
-
 describe('config', () => {
   it('throws when no authenticator is given', () => {
     expect(() =>
