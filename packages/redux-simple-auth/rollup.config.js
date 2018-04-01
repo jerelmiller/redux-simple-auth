c90c54f2fd5d1fc74bafe61d82601cdcc8ec1352
Jerel Miller <jerelmiller@gmail.com>
2018-04-01 16:54:59 -0700

    Add resolve plugin to config

--- packages/redux-simple-auth/rollup.config.js
+++ packages/redux-simple-auth/rollup.config.js
@@ -1,7 +1,10 @@
+import resolve from 'rollup-plugin-node-resolve'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'lib/index.js',
     format: 'cjs'
-  }
+  },
+  plugins: [resolve()]
 }
